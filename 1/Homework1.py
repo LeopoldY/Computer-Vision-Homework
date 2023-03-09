3e4b5e3c9f309f8bb460f293e8919f0631a7e2f3
Leopold Yang <1480335580@qq.com>
2023-03-09 13:46:22 +0800

    first

--- 1/Homework1.py
+++ 1/Homework1.py
@@ -3,7 +3,8 @@ import cv2
 from scipy import ndimage
 
 # 将路径修改为你的路径
-lena = cv2.imread("PATH/TO/lena.jpg", cv2.IMREAD_GRAYSCALE)
+lena = cv2.imread("1/lena.jpg", cv2.IMREAD_GRAYSCALE)
+blured = cv2.GaussianBlur(lena, (17, 17), 0)
 
 # sobel
 kernelSobel_X = np.array([[-1, 0, 1],
@@ -12,14 +13,14 @@ kernelSobel_X = np.array([[-1, 0, 1],
 kernelSobel_Y = np.array([[-1,-2,-1],
                           [ 0, 0, 0],
                           [ 1, 2, 1]])
-lenaSobel_X = ndimage.convolve(lena, kernelSobel_X)
-lenaSobel_Y = ndimage.convolve(lena, kernelSobel_Y)
+lenaSobel_X = ndimage.convolve(blured, kernelSobel_X)
+lenaSobel_Y = ndimage.convolve(blured, kernelSobel_Y)
 
 # Laplacian
 kernelLaplacian = np.array([[ 0, 1, 0],
                             [ 1,-4, 1],
                             [ 0, 1, 0]])
-lenaLaplacian = ndimage.convolve(lena, kernelLaplacian)
+lenaLaplacian = ndimage.convolve(blured, kernelLaplacian)
 
 # scharr
 kernalScharr_X = np.array([[ -3, 0,  3],
@@ -28,10 +29,11 @@ kernalScharr_X = np.array([[ -3, 0,  3],
 kernalScharr_Y = np.array([[ -3,-10, -3],
                            [  0,  0,  0],
                            [  3, 10,  3]])
-lenaScharr_X = ndimage.convolve(lena, kernalScharr_X)
-lenaScharr_Y = ndimage.convolve(lena, kernalScharr_Y)
+lenaScharr_X = ndimage.convolve(blured, kernalScharr_X)
+lenaScharr_Y = ndimage.convolve(blured, kernalScharr_Y)
 
 cv2.imshow("OG", lena)
+cv2.imshow("Blured", blured)
 cv2.imshow("Sobel_x", lenaSobel_X)
 cv2.imshow("Sobel_y", lenaSobel_Y)
 cv2.imshow("Laplacian", lenaLaplacian)
